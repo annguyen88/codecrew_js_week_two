012b90b2b6a87fc976d7e65bd94de76bc3e88b90
Max Jacobson <maxwell.jacobson@gmail.com>
2013-09-15 15:16:32 -0400

    adds another question and alerts them out

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
+
+
 $(document).ready(function() {
-  alert("HELLO!");
+
+  var allQuestions = [
+    {
+      question: "Who is Prime Minister of the United Kingdom?",
+      choices: ["David Cameron", "Gordon Brown", "Winston Churchill", "Tony Blair"], 
+      correctAnswer: 0
+    },
+    {
+      question: "What is the capitol of the United States?",
+      choices: ["Albany", "Philly", "NYC", "DC"],
+      correctAnswer: 3
+    }
+  ];
+
+  for(var i = 0; i < allQuestions.length; i++) {
+    alert(allQuestions[i].question + allQuestions[i].choices[allQuestions[i].correctAnswer]);
+  }
+
+
+
+  
+
+
 });
